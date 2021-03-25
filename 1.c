abac5a85a85ad2dc154a0a3acb6bcdbbe6a9a385
Piya <piya.rat@ku.th>
2021-03-25 19:55:04 +0700

    5

--- 1.c
+++ 1.c
@@ -3,11 +3,10 @@
     void main(){
         int n,i=2,j,k=0,x=0;
         scanf("%d",&n);
-        int no[100];
+        int no[1000];
         if(n>=1&&n<=1000000){
             while(i*i*i<=n){
             no[k]=i*i*i;
-            printf("%d  ",no[k]);
             k++;
             i++;
         }
@@ -18,10 +17,10 @@
             }
         }
         if(x!=0){
-            //printf("Not Cube Free");
+            printf("Not Cube Free");
         }
         else{
-            //printf("%d",n-k);
+            printf("%d",n-k);
         }
         }
 
