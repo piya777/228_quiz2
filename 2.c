cd51f6a61d23fda2892ff67871ed847753350667
Piya <piya.rat@ku.th>
2021-03-25 18:26:03 +0700

    2

--- 2.c
+++ 2.c
@@ -5,6 +5,13 @@
         scanf("%d",&m);
         scanf("%d",&n);
         char silk[m][70];
+        for( i=0 ; i<m ; i++ ){
+            for( j=0 ; j<70 ; j++ ){
+                silk[i][j]='o';
+                printf("%c",silk[i][j]);
+            }
+            printf("\n");
+        }
         for( i=0 ; i<n ; i++ ){
             scanf("%d %d %d",&p,&q,&r);
             for( j=q-1 ; j<(q-1)+r ; j++ ){
@@ -13,9 +20,6 @@
         }
         for( i=0 ; i<m ; i++ ){
             for( j=0 ; j<70 ; j++ ){
-                if(silk[i][j]==NULL){
-                    silk[i][j]='o';
-                }
                 printf("%c",silk[i][j]);
             }
             printf("\n");
