40b34f233e497615bb24cd66a0d38572b80fa3f1
Piya <piya.rat@ku.th>
2021-03-25 19:20:57 +0700

    3

--- 1.c
+++ 1.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
 
     void main(){
-
+        int n,i=2,j,k=0,x=0;
+        scanf("%d",&n);
+        int no[n];
+        while(1){
+            if(i*i*i>n){
+                break;
+            }
+            no[k]=i*i*i;
+            k++;
+            i++;
+        }
+        for( i=0 ; i<k ; i++ ){
+            if(n%no[i]==0){
+                x++;
+                break;
+            }
+        }
+        if(x!=0){
+            printf("Not Cube Free");
+        }
+        else{
+            printf("%d",n-k);
+        }
     }
